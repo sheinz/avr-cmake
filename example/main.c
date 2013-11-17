cc259ec29c0bfee4e17f90521c931b3e63403907
Yuriy Skriyka <yskriyka@luxoft.com>
2013-11-17 20:36:58 +0200

    Added fuses upload support

--- example/main.c
+++ example/main.c
@@ -3,6 +3,12 @@
 #include <avr/io.h>
 #include "test_lib.h"
 
+FUSES =
+{
+   .low = (FUSE_SUT0 & FUSE_CKSEL3 & FUSE_CKSEL1 & FUSE_CKSEL0),
+   .high = (FUSE_WDTON & FUSE_SPIEN & FUSE_CKOPT & FUSE_BOOTSZ1 & FUSE_BOOTSZ0),
+};
+
 int main(void)
 {
     DDRB |= _BV(PB0);
